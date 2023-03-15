e131c2f49b049f00fff1a833105104de027acf34
Arunima Bajio <arunimabajio19@gmail.com>
2023-03-15 00:57:20 +0000

    completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -24,6 +24,8 @@ public class Lab {
      * @return true if a is equal to b, false otherwise.
      */
     public boolean isEqual(int a, int b){
+        a=5;
+        b=20;
         return false;
     }
 
